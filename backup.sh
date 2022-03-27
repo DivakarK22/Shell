822c069013ddaa313927449c0b220a0d7cd58754
root <root@192.168.1.9>
2022-03-27 15:41:54 -0400

    Added new file

---
+++ backup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+Current=/home/slave-gui/simple-bash-scripts
+
+if [ $# != 1 ]; then
+  echo Enter two or more args 
+  exit 1
+fi
+
+DESTINATION=$1
+
+if [[ $DESTINATION != */backup ]]; then
+  echo Must include backup on path
+  exit 2
+fi
+
+mkdir -v -p $DESTINATION
+ 
+cp -v $Current/*.sh $DESTINATION
+
+
+echo "Backup was done sucessfully , Please check backup path"
+
+
+
+
