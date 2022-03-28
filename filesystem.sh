45d53889ca1cc33f9976fe5abf0073af98fcbcfc
root <root@localhost.localdomain>
2022-03-28 03:44:58 -0400

    New changes

--- filesystem.sh
+++ filesystem.sh
@@ -2,9 +2,26 @@
 
 file=$1
 
-if [ #! -ne 1 ];then
+if [ $# -ne 1 ]; then
   echo "Enter one Obj"
   exit 1
 fi
 
+if [ -f $file ]; then
+  READ=NOPE
+  WRITE=NOPE
+  EXE=NOPE
+
+   if [ -r $file ]; then
+     READ=YEAH
+   fi
+   if [ -w $file ]; then
+     WRITE=YEAH
+   fi
+   if [ -x $file ]; then
+    EXE=YEAH  
+   fi
+echo read : "$READ" wri  : "$WRITE" exe  : "$EXE"
+  
+fi
 
